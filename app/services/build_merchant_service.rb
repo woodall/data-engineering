19f58bdcb05a9537e95834bb4fa924cabacd5e60
Dave Woodall <dave@hireDave.me>
2013-12-05 13:05:27 -0700

    BuildMerchantService

---
+++ app/services/build_merchant_service.rb
@@ -0,0 +1,9 @@
+module BuildMerchantService
+  def self.call(data)
+    address = data[4]
+    name = data[5]
+
+    Merchant.where(merchant_address: address, merchant_name: name ).first_or_create
+  end
+end
+
---
+++ spec/services/build_merchant_service_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe BuildMerchantService do
+  it 'ensures only one record per merchant' do
+    data = [ 'n/a', 'n/a', 'n/a', 'Dave\'s Donuts', '1919 Colorado Building']
+    data_again = [ 'n/a', 'n/a', 'n/a', 'Dave\'s Donuts', '1919 Colorado Building']
+
+    donut_shop      = BuildMerchantService.call(data)
+    same_donut_shop = BuildMerchantService.call(data_again)
+
+    expect(Merchant.all.count).to eq(1)
+  end
+end
