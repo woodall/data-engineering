88627c199df433820c0ffb5ea5659a287bdc67cb
Dave Woodall <dave@hireDave.me>
2013-12-05 13:05:54 -0700

    DataNormalizationService

---
+++ app/services/data_normalization_service.rb
@@ -0,0 +1,13 @@
+module DataNormalizationService
+  def self.call file_contents
+    checkouts = []
+    checkout = file_contents.split(/\r?\n/)[1..-1]
+
+
+    checkout.each do |entry|
+      data = entry.split("\t")
+      checkouts << data
+    end
+    checkouts
+  end
+end
---
+++ spec/services/data_normalization_service_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe DataNormalizationService do
+  it 'passes' do
+    false
+  end
+end
