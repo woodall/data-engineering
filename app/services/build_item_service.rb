afa9a290b4184513c1b38af22d61f50d283b3043
Dave Woodall <dave@hireDave.me>
2013-12-05 13:37:53 -0700

    Ditch the inflation

--- app/services/build_item_service.rb
+++ app/services/build_item_service.rb
@@ -1,7 +1,7 @@
 module BuildItemService
   def self.call(data)
     description = data[1]
-    price = data[2].to_i * 100
+    price = data[2].to_i
 
     Item.where(item_description: description, item_price: price).first_or_create
   end
