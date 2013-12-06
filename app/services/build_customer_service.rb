07c34403f362219753462e41e5dcfc3486ffb351
Dave Woodall <dave@hireDave.me>
2013-12-05 19:14:07 -0700

    Simple error checking; Convert module to class

--- app/services/build_customer_service.rb
+++ app/services/build_customer_service.rb
@@ -1,7 +1,11 @@
-module BuildCustomerService
+class BuildCustomerService
 
   def self.call(data)
     customer = data[0]
-    Customer.where(name: customer).first_or_create
+    begin
+      Customer.where(name: customer).first_or_create
+    rescue
+      raise "You're not following the rules."
+    end
   end
 end
