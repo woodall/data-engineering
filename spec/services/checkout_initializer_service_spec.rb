3dedc0cdbe3bb884cfff2d16f4b61255cc9256e8
Dave Woodall <dave@hireDave.me>
2013-12-05 17:11:43 -0700

    Spec for CheckoutInitializerService

--- spec/services/checkout_initializer_service_spec.rb
+++ spec/services/checkout_initializer_service_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
 
 describe CheckoutInitializerService do
-  it 'passes' do
-    false
-    # data = [ 'Gilbert Arenas', 'n/a', 'n/a', 'n/a', 'n/a']
-    # data_again = [ 'Gilbert Arenas', 'n/a', 'n/a', 'n/a', 'n/a']
-
-    # gilbert         = BuildCustomerService.call(data)
-    # gilbert_returns = BuildCustomerService.call(data_again)
-
-    # expect(Customer.all.count).to eq(1)
+  it 'creates a Checkout object with the purchase count' do
+    checkout = FactoryGirl.create(:checkout)
+    purchase_count = checkout.purchase_count
+    expect(checkout).to be_valid
+    expect(purchase_count).to eq(2)
   end
 end
