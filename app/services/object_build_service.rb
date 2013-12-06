190ff519f949a55a4aeccc0065b22ecdbf86a576
Dave Woodall <dave@hireDave.me>
2013-12-05 17:10:23 -0700

    Spec and updates to ObjectBuildService

--- app/services/object_build_service.rb
+++ app/services/object_build_service.rb
@@ -2,12 +2,12 @@ module ObjectBuildService
 
   def self.call(data)
     data.inject([]) do |objects, order_info|
-      order_info = {
-        customer: BuildCustomerService.call(order_info),
-        item:     BuildItemService.call(order_info),
-        merchant: BuildMerchantService.call(order_info),
-        checkout: CheckoutInitializerService.call(order_info)
-        }
+      order_info = [
+          BuildCustomerService.call(order_info),
+          BuildItemService.call(order_info),
+          BuildMerchantService.call(order_info),
+          CheckoutInitializerService.call(order_info)
+        ]
       objects << order_info
     end
   end
