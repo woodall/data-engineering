18232800ad82ce972179a0bc2651c4180aa3db9e
Dave Woodall <dave@hireDave.me>
2013-12-05 17:11:58 -0700

    Spec for UploadCreationService

--- spec/services/upload_creation_service_spec.rb
+++ spec/services/upload_creation_service_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe UploadCreationService do
-  it 'doesnt fail' do
-    upload = UploadCreationService.call(data)
-    expect(upload).to be_valid
+  let(:upload)  { FactoryGirl.create(:upload) }
+  let(:ucs)     { UploadCreationService.call(file_contents, upload) }
+  let(:file_contents) { "purchaser name\titem description\titem price\tpurchase count\tmerchant address\tmerchant name\nSnake Plissken\t$10 off $20 of food\t10.0\t2\t987 Fake St\tBob's Pizza" }
+
+  it 'facilitates conversion of text files to Checkout objects' do
+    expect(ucs.count).to eq(1)
   end
 end
